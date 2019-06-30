a03405bf43a420f6f277c5009d4a19779b337205
DariaDubnikova <d.dasha20010513@gmail.com>
2019-06-30 15:49:47 +0300

    изменение

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
     if($text){
          if ($text == "/start") {
-            $reply = "Добро пожаловать в бота!";
+            $reply = "Добро пожаловать в бота!", $name;
             $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
         }
