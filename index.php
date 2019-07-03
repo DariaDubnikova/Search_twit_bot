40cf599efdea60541895a8d2c8ba50220a5f020e
DariaDubnikova <d.dasha20010513@gmail.com>
2019-07-03 15:57:07 +0300

    добавление redis

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
     include('vendor/autoload.php'); 
     use Telegram\Bot\Api; 
+    
+    require 'Predis/Autoloader.php';
+    Predis\Autoloader::register();
+
+    $redis = new Predis\Client(array(
+        'host' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_HOST),
+        'port' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PORT),
+        'password' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PASS),
+    ));
 
     $telegram = new Api('857128399:AAHHVAeKS_31miXbzVh4-1ZSzH2POkh0AyI'); 
     $result = $telegram -> getWebhookUpdates();
@@ -8,7 +17,7 @@
     $text = $result["message"]["text"];
     $chat_id = $result["message"]["chat"]["id"]; 
     $name = $result["message"]["from"]["username"]; 
-    $keyboard = [["Русский язык"]];
+    $keyboard = [];
     
     if($text){
          if ($text == "/start") {
@@ -33,27 +42,7 @@
                     'chat_id' => $chat_id, 
                     'text' => $reply ]); 
             }
-        } elseif ($text == "Русский язык") {
-             $reply = "Введите текст";
-                $telegram->sendMessage([ 
-                    'chat_id' => $chat_id, 
-                    'text' => $reply ]);
-             $baseUrl = 'http://api.voicerss.org/?';
-            $text = str_replace(' ','',$text); 
-             
-            $params = [
-                'key'=> 'b2da3917c24d458fbb6009689f2dfc9b',
-                'hl'=> 'ru-ru',
-                'src'=> $text, 
-                'c'=> 'mp3'
-            ];
-            $url = $baseUrl . http_build_query($params); 
-            
-        	$telegram->sendAudio([
-                'chat_id' => $chat_id,
-                'audio' => $url 
-            ]);
-         } else {
+        } else {
             $baseUrl = 'http://api.voicerss.org/?';
             $text = str_replace(' ','',$text); 
              
