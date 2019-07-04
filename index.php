436b5dd0a51751538dfc488a5212f462811a2918
DariaDubnikova <d.dasha20010513@gmail.com>
2019-07-04 15:06:44 +0300

    исправление

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         'password' => parse_url($_ENV['REDISCLOUD_URL'], PHP_URL_PASS),
     ));
 
-    $telegram = new Api('857128399:AAHHVAeKS_31miXbzVh4-1ZSzH2POkh0AyI'); 
+    $telegram = new Api('885313182:AAGshWA1PYDU_977cPuHh9BtEgwllFQpUSo'); 
     $result = $telegram -> getWebhookUpdates();
     
     $text = $result["message"]["text"];
@@ -19,52 +19,52 @@
  
     if($text){
          if ($text == "/start") {
-            $reply = "Добро пожаловать в бота!";
-            $reply_markup = $telegram->replyKeyboardMarkup([ 
-                'keyboard' => $keyboard, 
-                'resize_keyboard' => true, 
-                'one_time_keyboard' => false ]);
-            $telegram->sendMessage([ 
-                'chat_id' => $chat_id, 
-                'text' => $reply, 
-                'reply_markup' => $reply_markup ]);
-        } elseif ($text == "/sayhello") {
-            if (!empty($name)) {
-                $reply = "Привет, ". $name;
-                $telegram->sendMessage([ 
-                    'chat_id' => $chat_id, 
-                    'text' => $reply ]); 
-            } else {
-                $reply = "Привет, незнакомец";
-                $telegram->sendMessage([ 
-                    'chat_id' => $chat_id, 
-                    'text' => $reply ]); 
-            }
-        } elseif ($text == "Русский") {
+             $reply = "Добро пожаловать в бота!";
+             $reply_markup = $telegram->replyKeyboardMarkup([ 
+                 'keyboard' => $keyboard, 
+                 'resize_keyboard' => true, 
+                 'one_time_keyboard' => false ]);
+             $telegram->sendMessage([ 
+                 'chat_id' => $chat_id, 
+                 'text' => $reply, 
+                 'reply_markup' => $reply_markup ]);
+         } elseif ($text == "/sayhello") {
+             if (!empty($name)) {
+                 $reply = "Привет, ". $name;
+                 $telegram->sendMessage([ 
+                     'chat_id' => $chat_id, 
+                     'text' => $reply ]); 
+             } else {
+                 $reply = "Привет, незнакомец";
+                 $telegram->sendMessage([ 
+                     'chat_id' => $chat_id, 
+                     'text' => $reply ]); 
+             }
+         } elseif ($text == "Русский") {
              $redis->set($chat_id, 'ru-ru');
-        } elseif ($text == "English") {
+         } elseif ($text == "English") {
              $redis->set($chat_id, 'en-us');
          } else {
-            $baseUrl = 'http://api.voicerss.org/?';
-            $text = str_replace(' ','',$text);
-            
-            $lang = $redis->get($chat_id);
-            $lang = $lang ? $lang : 'en-us';
+             $baseUrl = 'http://api.voicerss.org/?';
+             $text = str_replace(' ','',$text);
              
-            $params = [
-                'key'=> 'b2da3917c24d458fbb6009689f2dfc9b',
-                'hl'=> $lang,
-                'src'=> $text,
-                'c'=> 'mp3'
-            ];
-            $url = $baseUrl . http_build_query($params); 
-            
-        	$telegram->sendAudio([
-                'chat_id' => $chat_id,
-                'audio' => $url 
-            ]);
+             $lang = $redis->get($chat_id);
+             $lang = $lang ? $lang : 'en-us';
+             
+             $params = [
+                 'key'=> 'b2da3917c24d458fbb6009689f2dfc9b',
+                 'hl'=> $lang,
+                 'src'=> $text,
+                 'c'=> 'mp3'
+             ];
+             $url = $baseUrl . http_build_query($params); 
+             
+             $telegram->sendAudio([
+                 'chat_id' => $chat_id,
+                 'audio' => $url 
+             ]);
          }
     }
     register_shutdown_function(function () {
-	   http_response_code(200);
+        http_response_code(200);
     });
